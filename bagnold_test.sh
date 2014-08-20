29ed9865e24b9d134dcd4e1ee4be7a91e10a1a08
Sachith Dunatunga <neocpp@gmail.com>
2014-08-20 02:41:47 -0400

    Added bagnold testing script.

---
+++ bagnold_test.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+
+MIN_SIZE=19
+STEP=1
+MAX_SIZE=34
+
+FLOWSTART=0.1
+
+MODEL_SO=g_local_mu2.so
+THREADS=1
+SIMTIME=10.0
+
+EMOD=1e9
+NUMOD=0.3
+
+# global preprocessing
+
+for opening_size in `seq $MIN_SIZE $STEP $MAX_SIZE`;
+do {
+    # preprocessing
+    OUTDIR=/home/$USER/backup/forward/bagnold-$opening_size
+    mkdir -p $OUTDIR
+
+    cat << EOF > $OUTDIR/runcfg
+timestep
+{
+    dt-max = 1e-2
+    dt-min = 1e-10
+    dt = 3e-6
+    automatic-dt = 0 
+    allow-dt-increase = 0
+    stable-dt-threshold = 4
+}
+
+solver
+{
+    solver-type = explicit-usl
+}
+
+material
+{
+    material-file = "./$MODEL_SO"
+    use-builtin = 0
+    properties = {$EMOD, $NUMOD}
+        # properties are Young's modulus and Poisson's ratio
+    integer-properties = { }
+        # no integer properties by default
+}
+
+boundary-conditions
+{
+    boundary-conditions-file = "builtin.so"
+    use-builtin = 1
+    properties = {-1, $FLOWSTART}
+    integer-properties = {$opening_size }
+}
+
+implicit
+{
+    displacement-norm-ratio = 1e-2
+    residual-norm-ratio = 1e-2
+
+    converged-displacement-norm = 1e-8
+
+    unstable-iteration-count = 10
+}
+
+input
+{
+    initial-particle-file = "generated_particles.txt"
+    grid-file = "generated_grid.txt"
+}
+
+output
+{
+    directory = "$OUTDIR/"
+    user = ${USER:-unknown}
+    sample-rate = 60.0
+}
+EOF
+
+    # run simulations
+    # echo $opening_size;
+    # ./mpm_2d -c $OUTDIR/runcfg -t $THREADS $SIMTIME
+
+    # postprocessing
+}; done;
+    OUTDIR=
+
+parallel --progress ./mpm_2d -c /home/$USER/backup/forward/bagnold-{}/runcfg -t$THREADS $SIMTIME  ::: `seq $MIN_SIZE $STEP $MAX_SIZE`;
