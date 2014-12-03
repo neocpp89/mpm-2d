5199898078c0db235cdbbe46d5cd17609c9931ee
Sachith Dunatunga <neocpp@gmail.com>
2014-12-03 16:36:12 -0500

    Fixed wrong prototypes for mapping functions.

--- libmpm/map.h
+++ libmpm/map.h
@@ -12,19 +12,18 @@
 
 /* double scalar arguments */
 void map_particles_to_nodes_doublescalar(job_t *job,
-    size_t node_field_offset, size_t particle_field_offset);
+    const size_t node_field_offset, const size_t particle_field_offset);
 void accumulate_p_to_n_doublescalar(node_t *nodes,
-    size_t node_field_offset, int *nodelist, double *sfvalues, 
-    int nodelist_len, double pdata);
+    const size_t node_field_offset, const int * restrict nodelist, const double * restrict sfvalues, 
+    const size_t nodelist_len, const double pdata);
 void accumulate_p_to_n_ds_list(node_t *nodes,
-    size_t *node_field_offset, int *nodelist, double *sfvalues, 
-    size_t nodelist_len, double *pdata, size_t pdata_len);
-
+    const size_t * restrict node_field_offset, const int * restrict nodelist, const double * restrict sfvalues, 
+    const size_t nodelist_len, const double * restrict pdata, const size_t pdata_len);
 void accumulate_p_to_n_ds_list47(node_t *nodes,
-    size_t *node_field_offset, int *nodelist, double *sfvalues, 
-    double *pdata);
+    const size_t * restrict node_field_offset, const int * restrict nodelist, const double * restrict sfvalues,
+    const double * restrict pdata);
 void accumulate_p_to_n_ds_list42(node_t *nodes,
-    size_t *node_field_offset, int *nodelist, double *sfvalues, 
-    double *pdata);
+    const size_t * restrict node_field_offset, const int * restrict nodelist, const double * restrict sfvalues, 
+    const double * restrict pdata);
 #endif
 
