190a3972977ddc9b97531e53e3bf16d99f0da776
Sachith Dunatunga <neocpp@gmail.com>
2015-08-28 10:58:03 -0400

    Added small script to prcoess com files.

---
+++ process_com.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+parallel --progress ./frame_to_csv.py {}/frame_particle_data.txt x,y,x_t,y_t,m 0:60 {}/fpd ::: drop/*.output
+parallel --progress "rm {}/com; for x in {0..59}; do ./com_finder_simple.py {}/fpd_\$x.csv >> {}/com; done;" ::: drop/*.output
+parallel "paste -d ' ' drop/{}_*.output/com > {}_com" ::: `seq 0 0.5 3`
